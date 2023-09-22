e7aa151a7d216ce23e61641cd4ec5839ff257185
Evangel <evangelopolot@gmail.com>
2023-09-22 11:22:30 +0100

    Test: Given a single phone log, return total cost.

--- src/main/java/uk/gov/dwp/BillCalculator.java
+++ src/main/java/uk/gov/dwp/BillCalculator.java
@@ -1,16 +1,16 @@
 package uk.gov.dwp;
 
 public class BillCalculator {
-    public int calculateBill(String s) {
-        String[] phoneLog = s.split(",");
+    public int calculateBill(String phoneLog) {
+        String[] phoneLogDetails = phoneLog.split(",");
 
-        String[] callDuration = phoneLog[0].split(":");
+        String[] callDuration = phoneLogDetails[0].split(":");
         int hours = Integer.parseInt(callDuration[0]);
         int minutes = Integer.parseInt(callDuration[1]);
         int seconds = Integer.parseInt(callDuration[2]);
 
         int callDurationInSeconds = (hours * 3600) + (minutes * 60) + seconds;
 
-        return callDurationInSeconds;
+        return callDurationInSeconds *  3;
     }
 }
--- src/test/java/PhoneBillCalculatorTest.java
+++ src/test/java/PhoneBillCalculatorTest.java
@@ -6,10 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class PhoneBillCalculatorTest {
     @Test
-    @DisplayName("Given a single phone log, return call duration in seconds")
-    public void givenASinglePhoneLogReturnCallDurationInSeconds(){
+    @DisplayName("Given a single phone log, return total cost")
+    public void givenASinglePhoneLogReturnTotalCost(){
         BillCalculator billCalculator = new BillCalculator();
         int result = billCalculator.calculateBill("00:01:07,400-234-090");
-        assertEquals("Should return 67 as call duration in seconds",67, result);
+        assertEquals("Should return 201 as total cost",201, result);
     }
+
 }
