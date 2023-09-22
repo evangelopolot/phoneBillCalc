9109efe60508ad07e245515e4f0ad75bbc217b70
Evangel <evangelopolot@gmail.com>
2023-09-22 13:21:36 +0100

    Test: Given multiple phone logs, return the total cost

--- src/main/java/uk/gov/dwp/BillCalculator.java
+++ src/main/java/uk/gov/dwp/BillCalculator.java
@@ -1,16 +1,55 @@
 package uk.gov.dwp;
 
+import java.util.HashMap;
+
 public class BillCalculator {
-    public int calculateBill(String phoneLog) {
-        String[] phoneLogDetails = phoneLog.split(",");
+    public int calculateBill(final String phoneLogInput) {
+        String[] phoneLogLines = phoneLogInput.split("\n");
 
-        String[] callDuration = phoneLogDetails[0].split(":");
-        int hours = Integer.parseInt(callDuration[0]);
-        int minutes = Integer.parseInt(callDuration[1]);
-        int seconds = Integer.parseInt(callDuration[2]);
+        HashMap<String, Integer> logs = new HashMap<>();
 
-        int callDurationInSeconds = (hours * 3600) + (minutes * 60) + seconds;
+        int longestCallDuration = 0;
+        String numberWithLongestCallDuration = "";
 
-        return callDurationInSeconds *  3;
+        for(String phoneLogLine : phoneLogLines){
+            String[] phoneLogDetails = phoneLogLine.split(",");
+            String[] timeSeparatedInParts = phoneLogDetails[0].split(":");
+            int hours = Integer.parseInt(timeSeparatedInParts[0]);
+            int minutes = Integer.parseInt(timeSeparatedInParts[1]);
+            int seconds = Integer.parseInt(timeSeparatedInParts[2]);
+            int callDurationInSeconds = (hours * 3600) + (minutes * 60) + seconds;
+
+            String phoneNumber = phoneLogDetails[1];
+            logs.put(phoneNumber, logs.getOrDefault(phoneNumber, 0) + callDurationInSeconds);
+
+            if(logs.get(phoneNumber) > longestCallDuration){
+                longestCallDuration = logs.get(phoneNumber);
+                numberWithLongestCallDuration = phoneNumber;
+
+            } else if(logs.get(phoneNumber) == longestCallDuration){
+
+                String currentNumber = phoneNumber.replaceAll("-","");
+                String longestDurationNumber = numberWithLongestCallDuration.replaceAll("-","");;
+                int currentNumberNumeric = Integer.parseInt(currentNumber);
+                int longestDurationNumberNumeric = Integer.parseInt(longestDurationNumber);
+                if(currentNumberNumeric < longestDurationNumberNumeric){
+                    numberWithLongestCallDuration = phoneNumber;
+                }
+            }
+        }
+        if (phoneLogLines.length > 1){
+            logs.put(numberWithLongestCallDuration, 0);
+        }
+
+        int totalCost = 0;
+        for(int duration : logs.values()){
+            if(duration <= 300){
+               totalCost += duration * 3;
+            } else {
+                totalCost += (int) Math.ceil(duration / 60.0) * 150;
+            }
+        }
+
+        return totalCost;
     }
 }
--- src/test/java/PhoneBillCalculatorTest.java
+++ src/test/java/PhoneBillCalculatorTest.java
@@ -13,4 +13,12 @@ public class PhoneBillCalculatorTest {
         assertEquals("Should return 201 as total cost",201, result);
     }
 
+    @Test
+    @DisplayName("Given multiple phone logs, return the total cost")
+    public void givenMultiplePhoneLogsReturnTotalCost(){
+        BillCalculator billCalculator = new BillCalculator();
+        int result = billCalculator.calculateBill("00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090");
+        assertEquals("Should return 900 as total cost", 900, result);
+    }
+
 }
