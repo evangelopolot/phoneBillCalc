8d337cad13d8548c9346df1a5c39475a4fa6f9b4
Evangel <evangelopolot@gmail.com>
2023-09-22 14:01:06 +0100

    Test: Refactored - Given multiple phone logs, return the total cost

--- src/main/java/uk/gov/dwp/BillCalculator.java
+++ src/main/java/uk/gov/dwp/BillCalculator.java
@@ -3,53 +3,75 @@ package uk.gov.dwp;
 import java.util.HashMap;
 
 public class BillCalculator {
+    private HashMap<String, Integer> logs = new HashMap<>();
+    private int longestCallDuration = 0;
+    private String numberWithLongestCallDuration = "";
+    private static final int HOUR_IN_SECONDS = 3600;
+    private static final int MINUTE_IN_SECONDS = 60;
+    private static final int RATE_FOR_SECONDS = 3;
+    private static final int RATE_FOR_MINUTES = 150;
+
     public int calculateBill(final String phoneLogInput) {
         String[] phoneLogLines = phoneLogInput.split("\n");
 
-        HashMap<String, Integer> logs = new HashMap<>();
-
-        int longestCallDuration = 0;
-        String numberWithLongestCallDuration = "";
-
         for(String phoneLogLine : phoneLogLines){
-            String[] phoneLogDetails = phoneLogLine.split(",");
-            String[] timeSeparatedInParts = phoneLogDetails[0].split(":");
-            int hours = Integer.parseInt(timeSeparatedInParts[0]);
-            int minutes = Integer.parseInt(timeSeparatedInParts[1]);
-            int seconds = Integer.parseInt(timeSeparatedInParts[2]);
-            int callDurationInSeconds = (hours * 3600) + (minutes * 60) + seconds;
+            processPhoneLog(phoneLogLine);
+        }
+        freePromotion(phoneLogLines);
+        return calculateTotalCost();
+    }
 
-            String phoneNumber = phoneLogDetails[1];
-            logs.put(phoneNumber, logs.getOrDefault(phoneNumber, 0) + callDurationInSeconds);
+    private void processPhoneLog(String phoneLogLine) {
+        String[] phoneLogDetails = phoneLogLine.split(",");
+        int callDurationsInSeconds = parseCallDuration(phoneLogDetails);
+        String phoneNumber = phoneLogDetails[1];
+        updateCallDurations(phoneNumber, callDurationsInSeconds);
+    }
 
-            if(logs.get(phoneNumber) > longestCallDuration){
-                longestCallDuration = logs.get(phoneNumber);
+    private int parseCallDuration(String[] phoneLogDetails){
+        String[] timeSeparatedInParts = phoneLogDetails[0].split(":");
+        int hours = Integer.parseInt(timeSeparatedInParts[0]);
+        int minutes = Integer.parseInt(timeSeparatedInParts[1]);
+        int seconds = Integer.parseInt(timeSeparatedInParts[2]);
+        return (hours * HOUR_IN_SECONDS) + (minutes * MINUTE_IN_SECONDS) + seconds;
+    }
+
+    private void updateCallDurations(String phoneNumber, int callDurationInSeconds){
+        logs.put(phoneNumber, logs.getOrDefault(phoneNumber, 0) + callDurationInSeconds);
+        updateLongestCall(phoneNumber);
+    }
+
+    private void updateLongestCall(String phoneNumber){
+        if(logs.get(phoneNumber) > longestCallDuration){
+            longestCallDuration = logs.get(phoneNumber);
+            numberWithLongestCallDuration = phoneNumber;
+
+        } else if(logs.get(phoneNumber) == longestCallDuration){
+
+            String currentNumber = phoneNumber.replaceAll("-","");
+            String longestDurationNumber = numberWithLongestCallDuration.replaceAll("-","");
+            int currentNumberNumeric = Integer.parseInt(currentNumber);
+            int longestDurationNumberNumeric = Integer.parseInt(longestDurationNumber);
+            if(currentNumberNumeric < longestDurationNumberNumeric){
                 numberWithLongestCallDuration = phoneNumber;
-
-            } else if(logs.get(phoneNumber) == longestCallDuration){
-
-                String currentNumber = phoneNumber.replaceAll("-","");
-                String longestDurationNumber = numberWithLongestCallDuration.replaceAll("-","");;
-                int currentNumberNumeric = Integer.parseInt(currentNumber);
-                int longestDurationNumberNumeric = Integer.parseInt(longestDurationNumber);
-                if(currentNumberNumeric < longestDurationNumberNumeric){
-                    numberWithLongestCallDuration = phoneNumber;
-                }
             }
         }
+    }
+
+    private void freePromotion(String[] phoneLogLines){
         if (phoneLogLines.length > 1){
             logs.put(numberWithLongestCallDuration, 0);
         }
-
+    }
+    private int calculateTotalCost(){
         int totalCost = 0;
         for(int duration : logs.values()){
             if(duration <= 300){
-               totalCost += duration * 3;
+                totalCost += duration * RATE_FOR_SECONDS;
             } else {
-                totalCost += (int) Math.ceil(duration / 60.0) * 150;
+                totalCost += (int) Math.ceil(duration / 60.0) * RATE_FOR_MINUTES;
             }
         }
-
         return totalCost;
     }
 }
